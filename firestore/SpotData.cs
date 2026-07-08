e417da3420b265fa53c7fcac1be791212043bb7f
teamiriam3-art <teamiriam3@gmail.com>
2026-07-08 10:10:13 +0900

    Create SpotData.cs

---
+++ firestore/SpotData.cs
@@ -0,0 +1,11 @@
+public class SpotData
+{
+    public string id { get; set; }
+    public string title_name { get; set; }
+    public double latitude { get; set; }
+    public double longitude { get; set; }
+    public string image_url { get; set; }
+    public string spot_info1 { get; set; }
+    public string spot_info2 { get; set; }
+    public string title_url { get; set; }
+}
